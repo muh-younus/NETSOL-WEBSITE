58d12b8f14307178c073aa4043b680a309e42712
muh-younus <braq369@gmail.com>
2024-07-07 14:30:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let sidebar = document.querySelector(".sidebar");
+let menu = document.querySelector(".menu");
+
+
+menu.addEventListener("click", () => {
+
+    sidebar.classList.toggle("show");
+
+});
