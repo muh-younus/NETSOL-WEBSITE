e931a7a41d5ae8ef8db0cacd448fec9987ebf32e
muh-younus <braq369@gmail.com>
2024-07-08 14:55:23 +0500

    updated file

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let sidebar = document.querySelector(".sidebar");
 let menu = document.querySelector(".menu");
+let line = document.querySelectorAll(".line");
+let text = document.querySelector(".toggleText");
 
 
 menu.addEventListener("click", () => {
 
+    if(text.innerHTML == "MENU"){
+        text.innerHTML = "CLOSE";
+    }
+    else{
+        text.innerHTML = "MENU";
+    }
+
     sidebar.classList.toggle("show");
+    line.forEach((item,index)=>{
+
+        line[0].classList.toggle("line-1");
+        line[1].classList.toggle("line-2");
+        line[2].classList.toggle("line-3");
+
+
+    });
 
 });
